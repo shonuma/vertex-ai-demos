5bd6661f95b7337d92e5cf4d6fe922784344f34e
Sho Onuma <sho@sonuma.dev>
2024-10-01 07:25:17 +0000

    remove print

--- controlled-generation-output/main.py
+++ controlled-generation-output/main.py
@@ -9,9 +9,6 @@ from vertexai.generative_models import GenerationConfig, GenerativeModel
 vertexai.init(project=os.environ['PROJECT_ID'], location="us-central1")
 
 def generate_content(prompt: str, schema: str):    
-
-    print(schema)
-
     if schema:
         response_schema = json.loads(schema)
     else:
@@ -37,7 +34,6 @@ def generate_content(prompt: str, schema: str):
             response_mime_type="application/json", response_schema=response_schema
         ),
     )
-    print(response.text)
     return response.text
 
 
